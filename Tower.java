f1b3c55da2c0282c6559f64ede2780a914ef61fa
Andrei Bayla <147172993+A-R-E-I@users.noreply.github.com>
2025-10-08 18:09:16 -0400

    Add files via upload

--- Tower.java
+++ Tower.java
@@ -24,36 +24,7 @@ public class Tower
 		System.out.print("Enter altitude(feet): ");
 		int alt = input.nextInt();
 		Airplane plane3 = new Airplane(cs, dist, dir, alt);
-		System.out.println("\nInitial Positions:");
-		System.out.println(plane1.toString());
-		System.out.println(plane2.toString());
-		System.out.println(plane3.toString());
-		System.out.println("\nInitial Distances:");
-		System.out.println("The distance between Airplane 1 and 2 is " + plane1.distTo(plane2) + " miles");
-		System.out.println("The distance between Airplane 1 and 3 is " + plane1.distTo(plane3) + " miles");
-		System.out.println("The distance between Airplane 2 and 3 is " + plane2.distTo(plane3) + " miles");
-		System.out.println("\nInitial Height Differences:");
-		System.out.println("The difference in height between Airplane 1 and 2 is " + plane1.getAlt(plane2) + " feet");
-		System.out.println("The difference in height between Airplane 1 and 3 is " + plane1.getAlt(plane3) + " feet");
-		System.out.println("The difference in height between Airplane 2 and 3 is " + plane2.getAlt(plane3) + " feet");
-		System.out.println("\nNew Positions:");
-		double distplane = plane2.distTo(plane3);
-		plane1.move(distplane, 65);
-		plane2.move(8.0, 135);
-		plane3.move(5.0, 55);
-		plane1.gainAlt(3000);
-		plane2.gainAlt(2000);
-		plane3.gainAlt(4000);
-		System.out.println(plane1.toString());
-		System.out.println(plane2.toString());
-		System.out.println(plane3.toString());
-		System.out.println("\nNew Distances:");
-		System.out.println("The distance between Airplane 1 and 2 is " + plane1.distTo(plane2) + " miles");
-		System.out.println("The distance between Airplane 1 and 3 is " + plane1.distTo(plane3) + " miles");
-		System.out.println("The distance between Airplane 2 and 3 is " + plane2.distTo(plane3) + " miles");
-		System.out.println("\nNew Height Differences:");
-		System.out.println("The difference in height between Airplane 1 and 2 is " + plane1.getAlt(plane2) + " feet");
-		System.out.println("The difference in height between Airplane 1 and 3 is " + plane1.getAlt(plane3) + " feet");
-		System.out.println("The difference in height between Airplane 2 and 3 is " + plane2.getAlt(plane3) + " feet");
+		Output display = new Output();
+		display.Outputs(plane1, plane2, plane3);
 	}
 }
